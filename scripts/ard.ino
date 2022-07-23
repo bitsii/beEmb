89417a5348d7c1696bb9caabbfc080dda55cf1c9
craig <craig@here.com>
2022-07-23 13:32:12 -0700

    update ard.ino

--- scripts/ard.ino
+++ scripts/ard.ino
@@ -1,23 +1,15 @@
-#define ONBOARD_LED  2
 #include "BEH_4_Base.hpp"
 
 void setup() {
-  pinMode(ONBOARD_LED,OUTPUT);
-  
-  Serial.begin(115200);                  
-  //Serial.print("Hello World"); 
+  Serial.begin(115200);   
+  //ESP.wdtDisable();
+  char* args[] = {(char*)""};
+  bems_relocMain(1, args);
+  //run app setup          
+  be::BEC_2_8_3_EmbeddedApp::bece_BEC_2_8_3_EmbeddedApp_bevs_inst->bem_startLoop_0();
 }
 
 void loop() {
-  delay(1000);
-  //Serial.print("Hi Loop"); 
-  //digitalWrite(ONBOARD_LED,HIGH);
-  //delay(500);
-  //digitalWrite(ONBOARD_LED,LOW);
-  char* args[] = {(char*)""};
-  bems_relocMain(1, args);
-  //delay(2000);
-  //digitalWrite(ONBOARD_LED,HIGH);
-  //delay(1000);
-  //digitalWrite(ONBOARD_LED,LOW);
+  //run app loop
+  be::BEC_2_8_3_EmbeddedApp::bece_BEC_2_8_3_EmbeddedApp_bevs_inst->bem_handleLoop_0();
 }
