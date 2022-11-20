bbc0340fa1e7fecafb48e8a50e1504127a96ec19
craig <craig@here>
2022-11-20 13:55:20 -0800

    bs starting

--- bsard/bsard.ino
+++ bsard/bsard.ino
@@ -1,7 +1,27 @@
+#include <ESP8266WiFi.h>
 
 void setup() {
   Serial.begin(115200);   
   //ESP.wdtDisable();
+  //ESP.wdtFeed();
+  //yield();
+  //delay(beva_millis->bevi_int);
+  //Serial.println("AP Started");
+  boolean result = WiFi.softAP("yoit_bs", "finn4321");
+  if(result == true)
+  {
+    Serial.println("AP Started");
+    String lip = WiFi.softAPIP().toString();
+    Serial.println(lip);
+    //std::string lips = std::string(lip.c_str());
+    //bevp_localIP = new BEC_2_4_6_TextString(lips);
+    //bevp_up = BECS_Runtime::boolTrue;
+  }
+  else
+  {
+    Serial.println("AP Start Failed!");
+    //bevp_up = BECS_Runtime::boolFalse;
+  }
 }
 
 void loop() {
