d1ac8ac16ce8dc93cd1e3559cf7214194fd2980f
craig <craig@here>
2023-07-31 01:32:49 -0700

    athomv2 working

--- system/cc/be/BEAR_Conf.hpp
+++ system/cc/be/BEAR_Conf.hpp
@@ -10,10 +10,10 @@
 // swconfver.DeviceTypeName.DeviceVersion
 //
 //#define BESPEC_SW "0.Generic.22"  //supergeneric
-//#define BESPEC_SW "0.NodeMcuLeds.24"  //nodemcu
+//#define BESPEC_SW "0.NodeMcuLeds.25"  //nodemcu
 //#define BESPEC_SW  "0.DollaLed.20" //dollatek 2mb 1led
-//#define BESPEC_SW "0.AthomPlugV2.26" //Athom Plug V2 US
-#define BESPEC_SW "0.NodeAthswy.25"  //nodemcu like athsw
+//#define BESPEC_SW "0.AthomPlugV2.29" //Athom Plug V2 US THE REAL ONE
+#define BESPEC_SW "0.NodeAthswy.27"  //nodemcu like athsw
 //
 // CON - definition of device's controls
 // ctlconfver.control.ctlconf,args.control.ctlconf,args
@@ -22,9 +22,12 @@
 //#define BESPEC_CON "0.sw.16.dim.2" //nodemcu
 //#define BESPEC_CON "0.sw.16"  //dollatek - one sw on 16
 //#define BESPEC_CON "0.dim.16"  //dollatek - one dim on 16
+//#define BESPEC_CON "0.sw.12,1.bu.5,0,30,0,30000"  //12 is switch, 13 is led, 5 button - athom plug v2 us - esp8266ex 2mb THE REAL ONE sic look unnecessary led does on it's own
 //#define BESPEC_CON "0.sw.12,1" //Athom Plug V2 US - sw on 12, 13 is led, default - same for martin jerry
 //#define BESPEC_CON "0.sw.12.sw.13"  //12 is switch, 13 is led - athom plug v2 us - esp8266ex 2mb
-#define BESPEC_CON "0.sw.2.bu.5,0,30,0,10000.sic.16,0,0" //nodemcu like athsw
+//#define BESPEC_CON "0.sw.2.bu.5,0,0,0,0" //nodemcu like athsw
+//#define BESPEC_CON "0.sw.2.bu.5,0,30,0,10000.sic.16,0,0" //nodemcu like athsw
+#define BESPEC_CON "0.sw.2.sic.16,0,0" //nodemcu like athsw
 //
 //pinposes //16, 2 nodemcu - Athom 16A US 13 LED 14 RELAY, SONOFF BASIC R2 13 LED 12 RELAY, 16 for dollatek 8285
 //
@@ -33,15 +36,15 @@
 // things like bulbs/less sensitive devices w/out another reasonable choice, it should be enabled.  device
 // waits a bit for the wifi to come up when configured  to avoid power losses leading to entering this mode
 //
-//#define BE_RESETBYPIN "on" //enabled
-#define BE_RESETBYPIN "off" //disabled
+#define BE_RESETBYPIN "on" //enabled
+//#define BE_RESETBYPIN "off" //disabled
 //
 // tcpconsole - if you want to enable a debugish tcp port on the device (all be prints will be echoed there)
 // turn BE_TCPCONSOLE on, otherwise leave off.  only for development, really only useful if serial connection
 // not available.  You can telnet to the device's ip port 32259 to see the messages.  output only, no input
 //
-//#define BE_TCPCONSOLE "on" //enabled
-#define BE_TCPCONSOLE "off" //disabled
+#define BE_TCPCONSOLE "on" //enabled
+//#define BE_TCPCONSOLE "off" //disabled
 //
 // END OF CONFIGURATION OPTIONS
 //
