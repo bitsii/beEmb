6b54c4fb2159d0144c57d58dd75180b79281dea6
craig <craig@here>
2023-08-09 17:35:01 -0700

    shorten typenames

--- system/cc/be/BEAR_Conf.hpp
+++ system/cc/be/BEAR_Conf.hpp
@@ -6,20 +6,22 @@
 // BESPEC_SW and BESPEC_CON can also be overridden on the device later
 // via commands (suggest starting with "supergeneric" options below)
 //
-// SW - name (type) and version of the device
+// SW - name (type) and version of the device.  Typename 10 char max ssid rules (no spaces)
 // swconfver.DeviceTypeName.DeviceVersion
 //
-//#define BESPEC_SW "0.Generic.22"  //supergeneric
-//#define BESPEC_SW "0.NodeMcuLeds.25"  //nodemcu
-//#define BESPEC_SW  "0.DollaLed.20" //dollatek 2mb 1led
-//#define BESPEC_SW "0.AthomPlugV2.29" //Athom Plug V2 US THE REAL ONE
-#define BESPEC_SW "0.NodeAthswy.27"  //nodemcu like athsw
+//#define BESPEC_SW "0.GEN.22"  //supergeneric
+#define BESPEC_SW "0.N.26"  //nodemcu
+//#define BESPEC_SW "0.NU.26"  //nodemcu unk name
+//#define BESPEC_SW  "0.DL.20" //dollatek 2mb 1led
+//#define BESPEC_SW "0.AP2.29" //Athom Plug V2 US THE REAL ONE
+//#define BESPEC_SW "0.NT.28"  //nodemcu like athsw
+//#define BESPEC_SW "0.NTU.28"  //nodemcu like athsw but unk name
 //
 // CON - definition of device's controls
 // ctlconfver.control.ctlconf,args.control.ctlconf,args
 //
 //#define BESPEC_CON "" //supergeneric
-//#define BESPEC_CON "0.sw.16.dim.2" //nodemcu
+#define BESPEC_CON "0.sw.16.dim.2" //nodemcu
 //#define BESPEC_CON "0.sw.16"  //dollatek - one sw on 16
 //#define BESPEC_CON "0.dim.16"  //dollatek - one dim on 16
 //#define BESPEC_CON "0.sw.12,1.bu.5,0,30,0,30000"  //12 is switch, 13 is led, 5 button - athom plug v2 us - esp8266ex 2mb THE REAL ONE sic look unnecessary led does on it's own
@@ -27,7 +29,7 @@
 //#define BESPEC_CON "0.sw.12.sw.13"  //12 is switch, 13 is led - athom plug v2 us - esp8266ex 2mb
 //#define BESPEC_CON "0.sw.2.bu.5,0,0,0,0" //nodemcu like athsw
 //#define BESPEC_CON "0.sw.2.bu.5,0,30,0,10000.sic.16,0,0" //nodemcu like athsw
-#define BESPEC_CON "0.sw.2.sic.16,0,0" //nodemcu like athsw
+//#define BESPEC_CON "0.sw.2.sic.16,0,0" //nodemcu like athsw
 //
 //pinposes //16, 2 nodemcu - Athom 16A US 13 LED 14 RELAY, SONOFF BASIC R2 13 LED 12 RELAY, 16 for dollatek 8285
 //
