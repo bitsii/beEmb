3138a1d6f00a225596e48a577094fe2fa7be5052
craig <craig@here>
2025-10-31 19:27:16 -0700

    s2 gmq

---
+++ scripts/pfgmqs322.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export APPBLDNM=pfgmqs322
+
+mkdir -p $APPBLDNM
+
+echo "Profile for a Esp32 NodeMCU Mqtt Gateway.  Serial input enabled." > $APPBLDNM/README.txt
+
+export BEEMB_CONF=../beEmb/confs/${APPBLDNM}.hpp
+
+./scripts/blde32gm.sh --mainClass Embedded:Nada source/Nada.be
+
+cp ard/* $APPBLDNM
+
+mv $APPBLDNM/ard.ino $APPBLDNM/${APPBLDNM}.ino
